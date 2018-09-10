1029362f710cecb992a41db7be382ca8e8aef030
qaisaru@icloud.com <qaisaru@icloud.com>
2018-09-10 18:55:00 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
-levitation_force = 6
+levitation_force=6
 def using_until
-while levitation_force < 10
+while levitation_force<10
   puts "Wingardium Leviosa"
-  levitation_force += 1
+  levitation_force+=1
 end
 end
